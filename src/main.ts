a3e87c722fba0fdddf12de14c4af819e521f1fef
Lautaro Ferreyra <lautaro.ferreyra@gigigo.com>
2026-04-22 18:09:31 -0300

    feat: add robots.txt and Vercel Analytics

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
+import { inject } from '@vercel/analytics'
 import './style.css'
 import App from './App.vue'
 
+inject()
 createApp(App).mount('#app')
 
 if ('serviceWorker' in navigator) {
